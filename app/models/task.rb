362f3b02b2cd644198cafd85b36a70c8be3ff355
banriikku <banriikku.ts@gmail.com>
2019-08-22 02:42:26 +0000

    タスク機能の編集(C11-C13)

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
-  #validates :name,  presence: true, length: { maximum: 50 }
-  #validates :description, presence: true, length: { maximum: 100 }
-  
+
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { in: 5..300 }
 	validates :user_id, presence: true
 
 end
