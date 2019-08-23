ae3b3a9b5d1803cd04fe068dbef08cddd548529a
banriikku <banriikku.ts@gmail.com>
2019-08-23 05:46:56 +0000

    D3-D8の編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,41 @@
 # coding: utf-8
 
-User.create!( name: "Sample User",
+User.create!( name: "管理者",
               email: "sample@email.com",
               password: "password",
-              password_confirmation: "password")
+              password_confirmation: "password",
+              admin: true)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "sample-#{n+1}@email.com"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
+
+50.times do |n|
+  task1 = "タスク#{n}"
+  task2 = "タスク詳細#{n}"
+  Task.create!(name: task1,
+               description: task2,
+               user_id: 1)
+end
+
+50.times do |n|
+  task1 = "タスク#{n}"
+  task2 = "タスク詳細#{n}"
+  Task.create!(name: task1,
+               description: task2,
+               user_id: 2)
+end
+
+50.times do |n|
+  task1 = "タスク#{n}"
+  task2 = "タスク詳細#{n}"
+  Task.create!(name: task1,
+               description: task2,
+               user_id: 3)
+end
